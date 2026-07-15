e06b472267106aa89f3a4f99fdae78dcaf2e96b3
hatchyz-coder <dispatch_hatchy@yahoo.co.jp>
2026-07-15 19:12:00 +0900

    Restore staging Pages build script

--- scripts/build-pages.sh
+++ scripts/build-pages.sh
@@ -9,9 +9,6 @@ cp self-pay.html _site/
 cp lhub.html _site/
 cp consultation.html _site/
 cp -R assets _site/assets
-if [ -f CNAME ]; then
-  cp CNAME _site/CNAME
-fi
 touch _site/.nojekyll
 
 echo "Built _site for GitHub Pages."
